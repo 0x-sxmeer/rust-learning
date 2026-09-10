07d2bb3281ef7b9755a6ae891e6040a44cf48d76
Sameer <20sameer90@gmail.com>
2026-09-10 10:27:49 +0530

    style: apply cargo fmt to variables-and-mutability

--- 01-basics/variables-and-mutability/src/main.rs
+++ 01-basics/variables-and-mutability/src/main.rs
@@ -1,20 +1,16 @@
-
 fn main() {
     let mut x = 6;
     println!("The value of x is:{}", x);
 
-    x = 7+4;
+    x = 7 + 4;
     println!("The value of x is:{}", x);
 
-{
-    const MAX_POINTS:u32 = 200_000; 
+    {
+        const MAX_POINTS: u32 = 200_000;
         println!("The value of MAX_POINTS is:{}", MAX_POINTS);
-
-}
+    }
 
     //Constant
-    const MAX_POINTS:u32 = 100_000;
+    const MAX_POINTS: u32 = 100_000;
     println!("The value of MAX_POINTS is:{}", MAX_POINTS);
-
 }
-
